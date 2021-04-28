c0dc9c04ad09ed058f21c02ead5e531c7424b0d2
Mike M <mikester70387@gmail.com>
2021-04-27 23:49:16 -0400

    Added status indicator to funny command

--- src/lib/modules/commands/types/fun/funny.ts
+++ src/lib/modules/commands/types/fun/funny.ts
@@ -4,7 +4,16 @@ import ensure from 'validator';
 import { execSync } from 'child_process';
 import { EmbedIconType } from '../../../../util';
 import { Message, MessageAttachment, User } from 'discord.js';
-import { Command, CommandReturn, CustomPermissions, emboss } from '@ilefa/ivy';
+
+import {
+    Command,
+    CommandReturn,
+    CustomPermissions,
+    emboss,
+    endLoader,
+    LOADER,
+    startLoader
+} from '@ilefa/ivy';
 
 export class FunnyCommand extends Command {
 
@@ -17,9 +26,7 @@ export class FunnyCommand extends Command {
             return CommandReturn.HELP_MENU;
         }
 
-        let url1 = args[0];
-        let url2 = args[1];
-
+        let [url1, url2] = args;
         if (!ensure.isURL(url1) || !ensure.isURL(url2)) {
             message.reply(this.embeds.build('Funny', EmbedIconType.TEST, ensure.isURL(url1)
                 ? ':link: Display URL is not valid.' 
@@ -27,23 +34,33 @@ export class FunnyCommand extends Command {
             return CommandReturn.EXIT;
         }
 
-        execSync(`mkdir tmp`);
-        execSync(`curl -o ./tmp/display.mp4 ${url1}`);
-        execSync(`curl -o ./tmp/second.mp4 ${url2}`);
-        execSync('ffmpeg -i ./tmp/second.mp4 -pix_fmt yuv444p ./tmp/second-new.mp4');
-        execSync('touch ./tmp/instruct.txt');
-        execSync('echo "file display.mp4\nfile second-new.mp4" >> ./tmp/instruct.txt');
-        execSync('ffmpeg -f concat -i ./tmp/instruct.txt -codec copy ./tmp/out.mp4');
-        execSync('rm ./tmp/second-new.mp4');
-        execSync('rm ./tmp/instruct.txt');
+        let loader = await startLoader(message);
+        
+        this.postStatus(loader.message, `${LOADER} Downloading assets..`);
+        this.exec(`mkdir tmp`);
+        this.exec(`curl -o ./tmp/display.mp4 ${url1}`);
+        this.exec(`curl -o ./tmp/second.mp4 ${url2}`);
+
+        this.postStatus(loader.message, `${LOADER} Encoding media..`);
+        this.exec('ffmpeg -i ./tmp/second.mp4 -pix_fmt yuv444p ./tmp/second-new.mp4');
+        this.exec('touch ./tmp/instruct.txt');
+        this.exec('echo "file display.mp4\nfile second-new.mp4" >> ./tmp/instruct.txt');
+        this.exec('ffmpeg -f concat -i ./tmp/instruct.txt -codec copy ./tmp/out.mp4');
+        this.exec('rm ./tmp/second-new.mp4');
+        this.exec('rm ./tmp/instruct.txt');
 
         let file = fs.readFileSync('./tmp/out.mp4');
         let attach = new MessageAttachment(file, 'out.mp4');
         
-        message.channel.send(attach);
-        execSync('rm -rf tmp');
+        this.postStatus(loader.message, `${LOADER} Uploading media..`);
+        message.channel.send(attach).then(_ => endLoader(loader));
+
+        this.exec('rm -rf tmp');
 
         return CommandReturn.EXIT;
     }
+
+    private exec = (cmd: string) => execSync(cmd, { stdio: 'ignore' });
+    private postStatus = (msg: Message, status: string) => msg.edit(status);
 
 }
