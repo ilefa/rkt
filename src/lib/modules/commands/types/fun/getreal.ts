5182e329b5cda04d4cdedb37e9c84ac292c465c1
Mike M <mikester70387@gmail.com>
2021-05-06 01:32:25 -0400

    Added new get real gifs & list command

--- src/lib/modules/commands/types/fun/getreal.ts
+++ src/lib/modules/commands/types/fun/getreal.ts
@@ -1,6 +1,16 @@
 import { select } from 'weighted-map';
-import { User, Message, Permissions } from 'discord.js';
-import { Command, CommandReturn, emboss } from '@ilefa/ivy';
+import { EmbedIconType } from '../../../../util';
+import { User, Message, Permissions, TextChannel } from 'discord.js';
+
+import {
+    bold,
+    Command,
+    CommandReturn,
+    CustomPermissions,
+    emboss,
+    link,
+    PaginatedEmbed
+} from '@ilefa/ivy';
 
 const weightedMap = new Map<string, number>()
     .set('https://tenor.com/view/get-real-trombone-cruel-angels-thesis-gif-20305175', 1)
@@ -16,7 +26,11 @@ const weightedMap = new Map<string, number>()
     .set('https://tenor.com/view/get-real-nintendo-funny-gif-18779041', 1)
     .set('https://tenor.com/view/get-real-get-real-thomas-thomas-gif-20099097', 1)
     .set('https://tenor.com/view/get-real-gif-20267230', 1)
-    .set('https://tenor.com/view/mcc-minecraft-championships-mc-championships-kaizodude-kaizo-gif-21249027', 1);
+    .set('https://tenor.com/view/mcc-minecraft-championships-mc-championships-kaizodude-kaizo-gif-21249027', 1)
+    .set('https://tenor.com/view/get-real-stickman-neon-dance-dancing-gif-20592187', 1)
+    .set('https://tenor.com/view/getreal-get-real-please-do-not-get-fake-gif-20594592', 1)
+    .set('https://tenor.com/view/sonic-the-hedgehog-sonic-get-real-get-real-gif-19386744', 1)
+    .set('https://tenor.com/view/get-real-fortnite-gif-20397038', 1);
 
 export class GetRealCommand extends Command {
     
@@ -25,12 +39,38 @@ export class GetRealCommand extends Command {
     }
     
     async execute(user: User, message: Message, args: string[]): Promise<CommandReturn> {
-        if (args.length !== 0) {
+        if (args.length > 1) {
             return CommandReturn.HELP_MENU;
+        }
+
+        if (args.length === 1 && !this.engine.has(user, CustomPermissions.SUPER_PERMS, message.guild)) {
+            return CommandReturn.HELP_MENU;
+        }
+
+        // hidden subcommand
+        if (args.length === 1 && args[0] === 'list') {
+            let arr = Array.from(weightedMap, ([gif, priority]) => ({ gif, priority }));
+            PaginatedEmbed.ofItems(this.engine, message.channel as TextChannel, user, 'Get Real', EmbedIconType.TEST, arr, 10, items => {
+                let list = `There are ${bold(weightedMap.size)} get real GIFs loaded and ready.\n\n${bold('Entries')}\n`;
+                items.forEach(ent => {
+                    list += ` • ${link(this._getDisplay(ent.gif), ent.gif)} (${ent.priority})\n`;
+                });
+    
+                return {
+                    description: list,
+                    fields: [],
+                }
+            })
+
+            return CommandReturn.EXIT;
         }
         
         message.channel.send(select(weightedMap));
         return CommandReturn.EXIT;
     }
+
+    private _getDisplay = (link: string) => link
+        .split('/view/')[1]
+        .split(/-(?:\d{8})/)[0];
 
 }
